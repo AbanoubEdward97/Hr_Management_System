222b98b3f55a76b8eaa4bad4de25287703582b6d
AbanoubEdward <abanoub.edward97101@gmail.com>
2024-03-25 13:25:48 +0200

    Initial commit

---
+++ HrSystemLastOne/Models/ITIContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace HrSystemLastOne.Models
+{
+    public class ITIContext : IdentityDbContext<ApplicationUser>
+    {
+        public ITIContext()
+        {
+            
+        }
+        public ITIContext(DbContextOptions options):base(options)
+        {
+            
+        }
+        public  DbSet<Department> Departments { get; set; }
+        public  DbSet<Employee> Employees { get; set; }
+
+        public  DbSet<General_Settings> General_Settings { get; set; }
+        public  DbSet<LeaveAttend> LeaveAttends { get; set; }
+        public  DbSet<OffDays> OffDays { get; set; }
+
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Xdb;Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
